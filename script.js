02be1497c4a9c6f8b8fa7421581822b83bdd7cdc
Zed Erevanayen <erevanayen>
2021-06-04 14:08:47 +0200

    Dark mode

--- script.js
+++ script.js
@@ -1,1 +1,30 @@
 console.log("I am running");
+
+var themeToggle = true;
+
+const logoEl = document.getElementById("logo");
+const cardEl = document.getElementById("card");
+var buttonsEl = document.getElementsByClassName("linkButton");
+logoEl.addEventListener("click", changeTheme);
+
+function changeTheme() {
+  if (themeToggle) {
+    //Light theme
+    logoEl.style.backgroundImage = `url("files/erevanayen_logo_BW2.png")`;
+    cardEl.style.backgroundColor = `white`;
+    cardEl.style.color = `black`;
+    for (let i = 0; i < buttonsEl.length; i++) {
+      buttonsEl[i].style.color = `black`;
+    }
+  } else {
+    logoEl.style.backgroundImage = `url("files/erevanayen_logo_base_trans.png")`;
+    cardEl.style.backgroundColor = `black`;
+    cardEl.style.color = `#FF105C`;
+    for (let i = 0; i < buttonsEl.length; i++) {
+      buttonsEl[i].style.color = `#FF105C`;
+    }
+  }
+  themeToggle = !themeToggle;
+}
+
+changeTheme();
