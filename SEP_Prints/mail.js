cdb0179b07a8517d58101c20691134a56d315526
zed.durotan@gmail.com <zed.durotan@gmail.com>
2021-06-25 18:08:15 +0200

    SEP_Prints done

---
+++ SEP_Prints/mail.js
@@ -0,0 +1,9 @@
+console.log("Mail built");
+
+var name = "zed";
+var name2 = "erevanayen";
+var dom = "art";
+
+const mailEl = document.getElementById("mail");
+
+mailEl.innerText = this.name + "@" + this.name2 + "." + this.dom;
